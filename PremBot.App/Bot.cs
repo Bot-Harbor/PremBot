db0e02c3e507fa904de71f14ef75fc5d59768751
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-16 09:52:17 -0500

    Configured bot

---
+++ PremBot.App/Bot.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using PremBot.App.Secrets;
+
+namespace PremBot.App;
+
+public abstract class Bot
+{
+    public static DiscordClient Client { get; set; }
+
+    public static async Task RunBotAsync()
+    {
+        var discord = new Discord();
+
+        var discordConfig = new DiscordConfiguration()
+        {
+            Intents = DiscordIntents.All,
+            Token = discord.Token,
+            TokenType = TokenType.Bot,
+            AutoReconnect = true
+        };
+
+        Client = new DiscordClient(discordConfig);
+
+        Client.Ready += Client_Ready;
+        
+        await Client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+
+    private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+    {
+        return Task.CompletedTask;
+    }
+}
--- PremBot.App/Program.cs
+++ PremBot.App/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using PremBot.App;
 
-Console.WriteLine("Hello, World!");
+await Bot.RunBotAsync();
