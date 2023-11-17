720e3978ee24cfce032568eb43c41a1d37de3d87
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-17 09:47:06 -0500

    Created enum for team ids

---
+++ PremBot.App/Enums/Team.cs
@@ -0,0 +1,45 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace PremBot.App.Enums;
+
+public enum Team
+{
+    [ChoiceName("Arsenal")]
+    Arsenal = 57,
+    [ChoiceName("Bournemouth")]
+    Bournemouth = 1044,
+    [ChoiceName("Brentford")]
+    Brentford = 402,
+    [ChoiceName("Brighton")]
+    Brighton = 397, 
+    [ChoiceName("Burnley")]
+    Burnley = 328,
+    [ChoiceName("Chelsea")]
+    Chelsea = 61,
+    [ChoiceName("Crystal Palace")]
+    CrystalPalace = 354,
+    [ChoiceName("Everton")]
+    Everton = 62,
+    [ChoiceName("Fulham")]
+    Fulham = 63,
+    [ChoiceName("Liverpool")]
+    Liverpool = 64,
+    [ChoiceName("Luton Town")]
+    LutonTown = 389,
+    [ChoiceName("Manchester City")]
+    ManchesterCity = 65,
+    [ChoiceName("Manchester United")]
+    ManchesterUnited = 66,
+    [ChoiceName("Newcastle")]
+    Newcastle = 67,
+    [ChoiceName("Nottingham Forest")]
+    NottinghamForest = 351,
+    [ChoiceName("Sheffield United")]
+    SheffieldUnited = 356,
+    [ChoiceName("Tottenham")]
+    Tottenham = 73,
+    [ChoiceName("West Ham")]
+    WestHam = 563,
+    [ChoiceName("Wolverhampton")]
+    Wolverhampton = 76
+}
