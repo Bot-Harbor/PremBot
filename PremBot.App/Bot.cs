8df7ed280f1a3d8f60d0f5f831e17b4fc925dd51
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-17 12:01:31 -0500

    Added help command

--- PremBot.App/Bot.cs
+++ PremBot.App/Bot.cs
@@ -45,5 +45,6 @@ public abstract class Bot
         slashCommands.RegisterCommands<CaptionCommand>();
         slashCommands.RegisterCommands<StandingsCommand>();
         slashCommands.RegisterCommands<FixtureCommand>();
+        slashCommands.RegisterCommands<HelpCommand>();
     }
 }
---
+++ PremBot.App/Slash Commands/HelpCommand.cs
@@ -0,0 +1,68 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace PremBot.App.Slash_Commands;
+
+public class HelpCommand : ApplicationCommandModule
+{
+    [SlashCommand("help", "Gives information about the bot & available commands.")]
+    public async Task HelpCommandAsync(InteractionContext context)
+    {
+        var helpEmbed = new DiscordEmbedBuilder()
+        {
+            Title = "📝 Getting Started",
+            Color = DiscordColor.White,
+            Description =
+                "Get information about the Premiere League! Type one of the commands below to get started. " +
+                $"PremBot powered by [DSharpPlus 4.4.2]" +
+                $"(https://dsharpplus.github.io/DSharpPlus/index.html) and [Docker](https://www.docker.com/).",
+        };
+
+        helpEmbed.AddField
+        (
+            "**⚽  Premiere League Commands**",
+            $"🏆 </standings:1174749725413617748>\n" +
+            $"📅  </fixture:1174832703904948234>",
+            inline: true
+        );
+
+        helpEmbed.AddField
+        (
+            "🛠️  Other Commands",
+            $"🆘  </help:1175112211291705457>\n" +
+            "🏓  </ping:1174725698007605278>\n" +
+            "📷 </caption:1174732729800208416>",
+            inline: true
+        );
+
+        var serverCount = context.Client.Guilds.Count;
+        var shardCount = context.Client.ShardCount;
+        var ping = context.Client.Ping;
+        var botVersion = context.Client.VersionString;
+
+        helpEmbed.WithFooter
+        ($"*Bot Info  •  " +
+         $"Total Servers: {serverCount}  •  " +
+         $"Shard: {shardCount}  •  " +
+         $"Ping: {ping}  •  " +
+         $"Version: {botVersion}"
+        );
+
+        var addButton = new DiscordLinkButtonComponent
+        (
+            "https://discord.com/api/oauth2/authorize?client_id=1174716762793721936&" +
+            "permissions=8&scope=bot%20applications.commands",
+            "Add PremBot To A Server"
+        );
+
+        var apiButton = new DiscordLinkButtonComponent("https://www.football-data.org/", "Utilize Soccer API");
+
+        var messageBuilder = new DiscordInteractionResponseBuilder {IsEphemeral = true,};
+        
+        messageBuilder.AddEmbed(helpEmbed);
+        messageBuilder.AddComponents(addButton, apiButton);
+
+        await context.CreateResponseAsync(messageBuilder);
+    }
+}
